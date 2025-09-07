4ad240b1fd86c0694b891076bd6b1e754f500bbb
Johnas Lopez <johnaslopez4@gmail.com>
2025-09-07 14:43:25 +0800

    new index.js

--- crud_backend/src/db.js
+++ crud_backend/src/db.js
@@ -3,12 +3,11 @@ import env from "dotenv";
 
 env.config();
 const db = new pg.Client({
-    user : process.env.PG_USER,
-    host : process.env.PG_HOST,
-    database : process.env.PG_DATABASE,
-    password : process.env.PG_PASSWORD,
-    port : process.env.PG_PORT
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // required on Render
 });
+
+db.connect();
 
 db.connect();
 db.on ("error", (err) => {
