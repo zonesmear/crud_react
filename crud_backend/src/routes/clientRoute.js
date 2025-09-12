c1453326ff0f3280371aff8cc845d7f0385fd5f9
Johnas Lopez <johnaslopez4@gmail.com>
2025-09-12 22:02:11 +0800

    backend fixed

--- crud_backend/src/routes/clientRoute.js
+++ crud_backend/src/routes/clientRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyToken } from "../middleware/authMiddleware.js";
-import * as clientController from "../controllers/clientController.js";
+import * as clientController from "../controllers/clientControllers.js";
 import * as authController from "../controllers/authController.js";
 
 const router = express.Router();
